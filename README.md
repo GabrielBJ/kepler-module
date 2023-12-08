5ad599da1b49137cb81be56b93c7f7b83941de17
GabrielBJ <gabrielbala2001@gmail.com>
2023-12-07 19:23:08 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+
+# This is a Python package to generate a two-body simulation 
+
+
+
+
+
+
+# How to run the code as a script 
+
+
+
+
+
+
+# How to use the code as a module 
+
+
+
+
+
+
+
+# Authors: 
+
+	- Gabriel Balarezo
+
+# Sample Notebooks
+
+
---
+++ setup.py
@@ -0,0 +1,13 @@
+# Import the libraries needed to run the setup 
+
+from setuptools import setup, find_packages
+
+setup(name = "odekepler",
+      description = "This produces a two-body simulation for the earth",
+      author = "J. Gabriel Balarezo",
+      author_email = "balarezog961@gmail.com",
+      license = "BSD",
+      version = "1.0",
+      packages = find_packages(),
+      install_requires = ['numpy', 'matplotlib', 'pillow', 'requests'])
+
